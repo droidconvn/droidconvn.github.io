8a024058a78e5269b203bdcded655a9bf5ecf7c5
Circle CI <rockerhieu+circleci@gmail.com>
2018-03-04 12:06:34 +0000

    New version 2018-03-04 12:06:34 - b011ddd53b8da0b1e0f0f2d1f6db0e31ad89654d

--- deploy.sh
+++ deploy.sh
@@ -1,56 +1,21 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
+COMMIT_SHA=$(git rev-parse HEAD)
+TMP_DIR=.tmp
+rm -rf $TMP_DIR
+mkdir -p $TMP_DIR
+cd $TMP_DIR
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
+git clone git@github.com:droidconvn/droidconvn.github.io.git
+cd droidconvn.github.io
+git checkout master || git checkout --orphan master
+cd ../..
 
-function doCompile {
-  ./compile.sh
-}
+cp -rf _site/* $TMP_DIR/droidconvn.github.io/
+cd $TMP_DIR/droidconvn.github.io
+git status
+git add -f *
 
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
-# Clean out existing contents
-rm -rf out/**/* || exit 0
-
-# Run our compile script
-doCompile
-
-# Now let's go have some fun with the cloned repo
-cd out
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add -A .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-
-# Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+COMMIT_MESSAGE=$(date "+%Y-%m-%d %H:%M:%S")
+git commit -m "New version $COMMIT_MESSAGE - $COMMIT_SHA"
+git push
